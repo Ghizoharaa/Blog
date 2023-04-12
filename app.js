1208d8d7bc51a21da87d5800872e6b7a3705e4f0
Ghizlane <khabir.ghizlane@ofppt-edu.ma>
2023-04-12 21:48:47 +0000

    Completed the blog in node.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes');
+
 //express app
 const app = express();
 
@@ -23,29 +24,20 @@ app.set('view engine', 'ejs');
 
 //Listen for request
 app.listen(3000);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
 app.use(morgan('dev'));
 
-// mongoose and mongo sandbox routes
-
-
-
+// routes
 app.get('/', (req, res) => {
-const blogs = [
-    { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit..'},
-    { title: 'Mario finds eggs', snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ' },
-    { title: 'How to defeat bowser', snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ' }
-    ];
-
-    res.render('index', {title: 'Home', blogs});
-});
-
+    res.redirect('/blogs');
+})
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 })
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create' });
-})
+// blog routes
+app.use(blogRoutes);
 
 app.use((req, res) => {
     res.status(404).render('404', { title: 'Error' });
